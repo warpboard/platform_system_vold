d9ab619f2404587eda845a7f5c1507a941734fa2
Blair Prescott <blairp@broadcom.com>
2013-10-21 15:25:37 -0700

    Use correct minor device number to format SD card
    When formatting SD card partitions, instead of assuming partition
    minor device numbers start from 1, use (minor device number of
    the disk device + 1) as the starting point.
    
    Buganizer: 11352632
    
    Change-Id: I69d37f57aec6b62fe2a055470f7525ec09dc3859
    Signed-off-by: Blair Prescott <blairp@broadcom.com>
    Signed-off-by: Pierre <pierre@broadcom.com>

--- Volume.cpp
+++ Volume.cpp
@@ -227,7 +227,9 @@ int Volume::formatVol() {
     bool formatEntireDevice = (mPartIdx == -1);
     char devicePath[255];
     dev_t diskNode = getDiskDevice();
-    dev_t partNode = MKDEV(MAJOR(diskNode), (formatEntireDevice ? 1 : mPartIdx));
+    dev_t partNode =
+        MKDEV(MAJOR(diskNode),
+              MINOR(diskNode) + (formatEntireDevice ? 1 : mPartIdx));
 
     setState(Volume::State_Formatting);
 
